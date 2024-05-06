96a1a6652947b5c132bec73cbd45fa6defc7ffb4
Dipak <dipakbsumesara@gmail.com>
2024-05-06 15:02:29 +0530

    test fixes

--- jest.config.ts
+++ jest.config.ts
@@ -2,4 +2,11 @@ import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
   projects: await getJestProjectsAsync(),
+  moduleNameMapper: {
+    // Assuming your backend and frontend are directly under the libs or apps.
+    '^@backend/(.*)$': '<rootDir>/backend/src/$1',
+    '^@frontend/(.*)$': '<rootDir>/frontend/src/$1',
+    // Add other necessary mappings based on your tsconfig paths
+  },
+  testEnvironment: 'node',
 });
